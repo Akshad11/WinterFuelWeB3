d4f87679b2edf98ab932f0de7cde41c35a34aef0
Akshad11 <akshad.dhole14.com>
2023-03-12 00:55:48 +0530

    wa

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import About from "./componet/page2/About.js"
 import MyQuest from "./componet/page3/MyQuest"
 import Quest from "./componet/page3/Quest"
-import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react';
+
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -14,12 +14,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <DynamicContextProvider
-          settings={{
-            environmentId: 'cf2ed575-790e-4904-a493-47e429ffe4e8',
-          }}>
-          <DynamicWidget />
-        </DynamicContextProvider>
+        <div id='meta'>
+
+
+        </div>
 
         <Router>
 
@@ -31,6 +29,8 @@ class App extends Component {
             <Route path='/Quest' element={<Quest />} />
           </Routes>
         </Router>
+
+
       </div>
 
 
