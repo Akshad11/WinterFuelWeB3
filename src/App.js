a65235aab4bf7037b7fcbe60b242eea07bd04a1c
Akshad11 <akshad.dhole14.com>
2023-03-12 00:09:30 +0530

    coonet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import About from "./componet/page2/About.js"
 import MyQuest from "./componet/page3/MyQuest"
 import Quest from "./componet/page3/Quest"
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,6 +14,12 @@ class App extends Component {
   render() {
     return (
       <div>
+        <DynamicContextProvider
+          settings={{
+            environmentId: 'cf2ed575-790e-4904-a493-47e429ffe4e8',
+          }}>
+          <DynamicWidget />
+        </DynamicContextProvider>
 
         <Router>
 
